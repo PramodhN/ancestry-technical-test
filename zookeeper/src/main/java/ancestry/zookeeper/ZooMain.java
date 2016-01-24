d0fcf8cc1285690a1438d4e5967b1877d22ed31d
pramodhn <pnatara7@asu.edu>
2016-01-23 17:54:30 -0700

    code for adding to new inventory and updating feed timings

--- zookeeper/src/main/java/ancestry/zookeeper/ZooMain.java
+++ zookeeper/src/main/java/ancestry/zookeeper/ZooMain.java
@@ -12,7 +12,6 @@ public class ZooMain {
 
 	private static int zooId;
 	private static Scanner in;
-	private static DBOperations operations;
 
 	public static void main(String[] args) {
 		in = new Scanner(System.in);
@@ -25,14 +24,15 @@ public class ZooMain {
 			choice1 = in.nextInt();
 			switch (choice1) {
 			case 1:
-				System.out.println("Options\n1. Add feed shipments to new inventory\n2. Record feed times for each animal\n3. Replace running with new inventory");
+				System.out.println("Options\n1. Add feed shipments to new inventory\n2. Record feed times for each animal\n3. Replace running with new inventory"
+						+ "\nEnter your choice");
 				choice2 = in.nextInt();
 				performInventoryUpdate(choice2);
 				break;
 			case 2:
 				System.out.println("Options\n1. How much was each individual animal fed per day on average?\n2. How many times per day are animals fed on average?"
 						+ " Group by species.\n3. How	much food is wasted per	zoo?\n4. Which species of animal at which zoos are being fed above/below "
-						+ "average(by species) by some percentage?");
+						+ "average(by species) by some percentage?\nEnter your choice");
 				choice2 = in.nextInt();
 				getStats(choice2);
 				break;
@@ -47,23 +47,26 @@ public class ZooMain {
 		}
 	}
 
-	private static void getStats(int choice) {
+	private static void performInventoryUpdate(int choice) {
 		switch (choice) {
 		case 1:
 			System.out.print("Enter feed shipment quantity: ");
 			double newQuantity = in.nextInt();
-
+			DBOperations.addNewQuantity(zooId, newQuantity);
 			break;
 		case 2:
+			DBOperations.recordFeedTime(zooId, in);
 			break;
 		case 3:
 			break;
+
 		default:
 			System.out.println("Incorrect option chosen!");
+
 		}
 	}
 
-	private static void performInventoryUpdate(int choice) {
+	private static void getStats(int choice) {
 		switch (choice) {
 		case 1:
 			break;
@@ -75,8 +78,6 @@ public class ZooMain {
 			break;
 		default:
 			System.out.println("Incorrect option chosen!");
-
 		}
 	}
-
 }
--- zookeeper/src/main/java/ancestry/zookeeper/db/DBOperations.java
+++ zookeeper/src/main/java/ancestry/zookeeper/db/DBOperations.java
@@ -1,5 +1,13 @@
 package ancestry.zookeeper.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import ancestry.zookeeper.model.Animal;
 import ancestry.zookeeper.model.Zoo;
 
@@ -10,6 +18,47 @@ public class DBOperations {
 	public DBOperations() {
 		zoo = new Zoo();
 		animal = new Animal();
+	}
+
+	public static void addNewQuantity(int zooId, double newQuantity) {
+		String updateQuery = "UPDATE zoo SET new_inventory = new_inventory+" + newQuantity + " WHERE zooid = " + zooId;
+		Connection con = (Connection) DBConnectivity.getInstance();
+		try {
+			PreparedStatement preparedStmt = (PreparedStatement) con.prepareStatement(updateQuery);
+			preparedStmt.execute();
+			System.out.println("New shipment has been updated!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void recordFeedTime(int zooId, Scanner in) {
+		String selectAnimalsQuery = "SELECT * FROM animal WHERE zooid = " + zooId;
+		Connection con = (Connection) DBConnectivity.getInstance();
+		Statement statement;
+		try {
+			statement = con.createStatement();
+			ResultSet rs = statement.executeQuery(selectAnimalsQuery);
+			String animalId, species;
+			ArrayList<String> updateQueries = new ArrayList<String>();
+			while (rs.next()) {
+				animalId = rs.getString("animalid");
+				species = rs.getString("species");
+				System.out.println("Enter feed time for " + species + "(id:" + animalId + "): ");
+				int feedTime = in.nextInt();
+				System.out.println("Enter feed quantity for " + species + "(id:" + animalId + "): ");
+				double feedQty = in.nextDouble();
+				String updateQuery = "UPDATE animal SET feedtime = " + feedTime + ", quantity = " + feedQty + " WHERE animalId = " + animalId;
+				updateQueries.add(updateQuery);
+			}
+			for (String updateQuery : updateQueries) {
+				PreparedStatement preparedStmt = (PreparedStatement) con.prepareStatement(updateQuery);
+				preparedStmt.execute();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
